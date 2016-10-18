f4e0e6d7dc6b292371783f886ac64ab57f06b7d9
zhaokaiyuan <zhaoky0918@gmail.com>
2016-10-18 17:36:39 +0800

    mod

--- www/alipay.js
+++ www/alipay.js
@@ -24,18 +24,13 @@ Alipay.prototype.settings = {
 Alipay.prototype.getOrderInfo = function(opts) {
     var _this = this;
     var opts = this.merge({
-        out_trade_no: new Date().getTime(),
-        subject: '商品标题',
-        body: '商品描述',
-        total_fee: '0.01',
-        notify_url: 'http://m.alipay.com',
-        return_url: ''
+
     }, opts);
 
-    var params = 'partner="' + this.settings.partner + '"&seller_id="' + this.settings.seller_id + '"&service="mobile.securitypay.pay"&_input_charset="UTF-8"&payment_type="1"&it_b_pay="1m"';
+    var params = 'partner="' + this.settings.partner + '"&seller_id="' + this.settings.seller_id + '"&service="mobile.securitypay.pay"&_input_charset="UTF-8"&payment_type="1"';
 
     for (var k in opts) {
-        params += '&' + k + '="' + encodeURI(opts[k]) + '"';
+        params += '&' + k + '="' + (opts[k]) + '"';
     };
 
     if (opts.debug) {
